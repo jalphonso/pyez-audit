ca3992386d24da3b48e6e0ac991547995d34f99c
Joe Alphonso <jalphonso@juniper.net>
2018-10-25 20:28:13 -0400

    created new table and fixed implementation to fix bugs

--- check_if_transceivers_are_configured.py
+++ check_if_transceivers_are_configured.py
@@ -4,6 +4,7 @@ from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
 from jnpr.junos.op.xcvr import XcvrTable
 from myTables.ConfigTables import InterfaceTable
+from myTables.OpTables import EthPortTable
 
 
 def main():
@@ -16,15 +17,10 @@ def main():
 
             config_interfaces = InterfaceTable(dev).get()
             xcvrs = XcvrTable(dev).get()
-            for xcvr in xcvrs:
-                xcvr_fpc = xcvr.name[0].split()[1]
-                xcvr_pic = xcvr.name[1].split()[1]
-                xcvr_port = xcvr.name[2].split()[1]
-                xcvr_name = xcvr_fpc + "/" + xcvr_pic + "/" + xcvr_port
-                xcvr_regex = xcvr_name + "($|:[\d])"
-                xcvr_compiled_regex = re.compile(xcvr_regex)
-                if not any(xcvr_compiled_regex.search(interface.name) for interface in config_interfaces):
-                    print("Xcvr {xcvr} is not configured".format(xcvr=xcvr_name))
+            eths = EthPortTable(dev).get()
+            for eth in eths:
+                if not any(eth.name == interface.name for interface in config_interfaces):
+                    print("{eth} is not configured".format(eth=eth.name))
         print("Finished configured interface audit of device {hostname}".format(hostname=hostname))
     except ConnectError as err:
         print ("Cannot connect to device: {0}".format(err))
