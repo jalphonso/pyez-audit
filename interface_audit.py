560e49508917f505d0840fdc1f7052245f80ac95
Joe Alphonso <jalphonso@juniper.net>
2018-10-29 13:22:12 -0400

    added logic to check for disabled port signature

--- interface_audit.py
+++ interface_audit.py
@@ -18,20 +18,24 @@ def main():
             eths = EthPortTable(dev).get()
             print("################UNCONFIGURED INTERFACES################")
             for eth in eths:
-                if (not eth.name in config_interfaces.keys() or (not config_interfaces[eth.name].unit
+                if(not eth.name in config_interfaces.keys() or (not config_interfaces[eth.name].unit
                     and not config_interfaces[eth.name].etherlag and not config_interfaces[eth.name].gigetherlag)):
                     print("Interface: {}\nSpeed: {}\nOper-status: {}\nAdmin-status: {}\n"\
                           "Description: {}\nLink-mode: {}\nMedia-type: {}\nInt-type: {}\n"\
                           "Mac Addr: {}\n".format(eth.name, eth.speed, eth.oper, eth.admin,
                                                   eth.description, eth.link_mode, eth.media_type,
                                                   eth.int_type, eth.macaddr))
+                elif(eth.name in config_interfaces.keys() and config_interfaces[eth.name].unit and
+                     config_interfaces[eth.name].phy_disable and config_interfaces[eth.name].unit_disable
+                     and config_interfaces[eth.name].vlan == 'badvlan'):
+                    print(eth.name + " was probably disabled by a script")
         print("##########################END##########################")
         print("Finished configured interface audit of device {hostname}".format(hostname=hostname))
     except ConnectError as err:
-        print ("Cannot connect to device: {0}".format(err))
+        print("Cannot connect to device: {0}".format(err))
         sys.exit(1)
     except Exception as err:
-        print (err)
+        print(err)
         sys.exit(1)
 
 if __name__ == "__main__":
